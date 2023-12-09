2cd663ecd8745c30140949223f8bf0020d0b39a5
zakisw <youssefakisw@gmail.com>
2023-12-09 06:19:58 -0800

    ol

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 number = 98
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-#!/usr/bin/python3
-print("{:d} Battery street".format(number))
+print('{0} Battery street'.format(number))

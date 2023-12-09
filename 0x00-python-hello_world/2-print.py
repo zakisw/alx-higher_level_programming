d25fb7c26d9cc332e610ccc4b19a966d12f72665
zakisw <youssefakisw@gmail.com>
2023-12-09 06:14:41 -0800

    ol

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-print('\'"Programming is like building a multilingual puzzle')
-=======
-print("Programming is like building a multilingual puzzle\n")
->>>>>>> 8b8da9bf89d65fb3d5b23aaf2abd47b5b0a3af60
+print('\"Programming is like building a multilingual puzzle')

2914a58fc33984360211cddd0d0f08e55cb9948c
zakisw <youssefakisw@gmail.com>
2023-12-10 07:54:42 -0800

    ololo

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first = word[0:3]
+second = word[-2:]
+middle = word[1:8]
+print(f"First 3 letters: {first}")
+print(f"Last 2 letters: {second}")
+print(f"Middle word: {middle}")

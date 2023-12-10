44f540af072c0726ea9f44eba9203fd85bbddaaf
zakisw <youssefakisw@gmail.com>
2023-12-10 07:58:09 -0800

    ololo

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 first = word[:3]
 second = word[-2:]
-middle = word[1:8]
+middle = word[1:-1]
 print(f"First 3 letters: {first}")
 print(f"Last 2 letters: {second}")
 print(f"Middle word: {middle}")

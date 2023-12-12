7ec5a3c36f5b7aa6703bb1b1dc394a2af3221d4d
zakisw <youssefakisw@gmail.com>
2023-12-12 07:23:27 -0800

    ok

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0,9):
     for j in range (0,9):
-        print('{}{} ,'.format(i,j), end='')
+        if i == j:
+            pass
+        else:
+            print('{}{} ,'.format(i,j), end='')

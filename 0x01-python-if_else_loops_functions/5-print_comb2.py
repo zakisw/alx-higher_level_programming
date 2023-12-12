914a1df35fc3fecd13680d0d6187b33593457770
zakisw <youssefakisw@gmail.com>
2023-12-12 07:07:57 -0800

    ol

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if 0 <= i < 10:
-        print('0{}, '.format(i), end='')
-
-    else:
-        print('{}, '.format(i), end='')
+for i in range(0, 99):
+    print('{:02d}, '.format(i), end='')
+print('99')

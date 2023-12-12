61f6aaaaafbab7c9f6eb61de5351087411a3c68a
zakisw <youssefakisw@gmail.com>
2023-12-12 14:51:34 -0800

    ol

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(65,122):
-    if 65<= i <= 90:
+for i in range(123):
+
+    if( 65 <= i) and (i <= 90):
         print('{} is upper'.format(chr(i)))
-    elif 97 <= i <= 122:
+    elif (97 <= i <= 122):
         print('{} is lower'.format(chr(i)))

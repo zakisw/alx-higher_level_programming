6e31f4a78ed672a1e1e1fd2b8826e20ec612dcbb
zakisw <youssefakisw@gmail.com>
2023-12-20 06:54:24 -0800

    ol

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,11 +2,11 @@
 '''
     __init__: does nothing
 '''
+
+
 class Square:
     '''
     This is an empty class Square that defines a square
     '''
-    def __int__(self, name = ''):
+    def __int__(self, name=''):
         self.name = square
-
-

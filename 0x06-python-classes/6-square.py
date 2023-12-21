76384323eb1e4f5924b46f4e969730a1d9c4d685
zakisw <youssefakisw@gmail.com>
2023-12-21 07:47:31 -0800

    ol

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,6 +56,7 @@ class Square:
     def position(self, value):
         if not isinstance (value, tuple) and value >=0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
     def my_print(self):
         '''
         prints in stdout the square with the character #

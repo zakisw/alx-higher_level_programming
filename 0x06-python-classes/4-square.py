ca3387566eb973b2579591dba31881669a4f03a3
zakisw <youssefakisw@gmail.com>
2023-12-21 02:47:24 -0800

    ol

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -31,10 +31,3 @@ class Square:
         """Return the current area of the square."""
         return (self.__size * self.__size)
 
-    def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
-        if self.__size == 0:
-            print("")

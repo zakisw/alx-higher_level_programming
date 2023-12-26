bd8ff951777dd15e6087102fdbbf5fc40ee64f8f
zakisw <youssefakisw@gmail.com>
2023-12-26 06:34:00 -0800

    ol

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_arguments():
+    # Total number of arguments
+    # Subtract 1 to exclude the script name itself
+    total_args = len(sys.argv) - 1
+
+    # List of arguments
+    args_list = sys.argv[1:]  # Exclude the script name
+    if total_args <= 0:
+        print("{} arguments.".format(total_args))
+    else:
+        print("{} arguments:".format(total_args))
+        print(f"{total_args}: {' '.join(args_list)}")
+
+
+if __name__ == "__main__":
+    print_arguments()

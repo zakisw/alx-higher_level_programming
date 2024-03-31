472604e6f1285c6a9c67e0d0ac85fb7e96180e8d
zakisw <youssefakisw@gmail.com>
2024-03-31 11:03:53 -0700

    o

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        l = 1
+        idx = 1
         for j in i:
-            if l == len(i):
+            if idx == len(i):
                 print("{:d}".format(j), end="")
             else:
                 print("{:d}".format(j), end=" ")
-            l = l + 1
+            idx = idx + 1
         print()

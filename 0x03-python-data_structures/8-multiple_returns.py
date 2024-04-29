2e59b8e63b8b8c1045df7749ea80dd76664ab20c
zakisw <youssefakisw@gmail.com>
2024-04-29 04:02:15 -0700

    lo

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,10 +3,10 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         x = len(sentence)
         y = 'None'
-        tuple_1 = x,y
+        tuple_1 = x, y
         return tuple_1
     else:
         x = len(sentence)
         y = sentence[0]
-        tuple_1 = x,y
+        tuple_1 = x, y
         return tuple_1

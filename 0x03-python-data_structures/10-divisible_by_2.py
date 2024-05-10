444a463e233953a97ba2ab362ff78f2c7e76d810
zakisw <youssefakisw@gmail.com>
2024-05-10 07:49:02 -0700

    looo

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    result_list = []
+    for i in my_list:
+        if i%2 == 0:
+            result_list.insert(0, True)
+        else:
+            result_list.insert(0, False)
+    return result_list

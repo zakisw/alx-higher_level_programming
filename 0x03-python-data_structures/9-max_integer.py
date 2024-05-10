2fa043e59b973f803ab0b8d271171771b2252f2f
zakisw <youssefakisw@gmail.com>
2024-05-10 03:20:53 -0700

    ki

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list == []:
-            return None
+        return None
     elif len(my_list) == 1:
         return my_list[0]
     else:
         maxx = my_list[0]
         for j in my_list[1:]:
-            if  j > maxx:
+            if j > maxx:
                 maxx = j
-        return maxx   
-                    
-
+        return maxx

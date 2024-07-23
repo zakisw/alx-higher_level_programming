bc323a185c8374c1adb6b91c89813963fa7ec1e7
zakisw <youssefakisw@gmail.com>
2024-07-23 04:10:43 -0700

    lo

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []  # Initialize new_matrix as an empty list
+
+    for row in matrix:
+        square_row = []
+        for i in row:
+            square_row.append(i*i)
+        new_matrix.append(square_row)
+    return new_matrix
